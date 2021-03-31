f30259629bffa3c092bbac84c6f9276a3780c712
Andriy <andriy.pidhorny@gmail.com>
2021-03-31 17:42:52 +0300

    210331 update all site

--- helpPage.js
+++ helpPage.js
@@ -8,26 +8,39 @@ const li2 = document.querySelector('.li-2');
 const li3 = document.querySelector('.li-3');
 const li4 = document.querySelector('.li-4');
 
-const closeFotoBtn1 = document.querySelector('.closeBtn');
-const closeFotoBtn2 = document.querySelector('.closeBtn2');
+// ========== CLOSE FOTO ==========
+const fotos = document.querySelectorAll('.foto');
 
-// !MODAL-1
-// const openModalFoto = document.querySelector('.foto-1');
-// const closeModalFoto = document.querySelector('.modal');
-// const modal = document.querySelector('.modal');
+function closeFoto() {
+    fotos.forEach((foto) => {
+        foto.classList.add('closeFoto');
+        foto.classList.remove('openFoto');
+        setTimeout(() => {
+            foto.classList.remove('closeFoto');
+        }, 0010);
+    });
+}
+//========== CLOSE FOTO VIA BTN ==========
+const closeBtns = document.querySelectorAll('.closeBtn');
 
-// openModalFoto.addEventListener('click', open);
-// closeModalFoto.addEventListener('click', close);
+closeBtns.forEach((btn) => {
+    btn.addEventListener('click', closeFoto);
+});
+// ========== CLOSE FOTO VIA ESCAPE ==========
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape') {
+        closeFoto();
+    }
+});
+// ========== CLOSE FOTO VIA EMPTY CLICK (doesn't work)==========
+// const sidebar = document.querySelector('.content-sidebar');
+// sidebar.addEventListener('click', (e) => {
+//     if (e.target !== fotos) {
+//         closeFoto();
+//     }
+// });
+// ========== /CLOSE FOTO ==========
 
-// function open() {
-//     modal.classList.add('show');
-//     modal.classList.remove('hide');
-// }
-// function close() {
-//     modal.classList.add('hide');
-//     modal.classList.remove('show');
-// }
-// ! /MODAL-1
 //! OPEN FOTO
 foto1.addEventListener('click', () => {
     if (!foto1.classList.contains('closeFoto')) {
@@ -49,44 +62,6 @@ li1.addEventListener('click', () => {
         foto2.classList.remove('closeFoto');
     }, 0010);
 });
-
-closeFotoBtn1.addEventListener('click', () => {
-    foto1.classList.add('closeFoto');
-    foto1.classList.remove('openFoto');
-    setTimeout(() => {
-        foto1.classList.remove('closeFoto');
-    }, 0010);
-});
-
-// Escape CLose
-document.addEventListener('keydown', (e) => {
-    if (e.code === 'Escape') {
-        foto1.classList.add('closeFoto');
-        foto1.classList.remove('openFoto');
-        setTimeout(() => {
-            foto1.classList.remove('closeFoto');
-        }, 0010);
-
-        foto2.classList.add('closeFoto');
-        foto2.classList.remove('openFoto');
-        setTimeout(() => {
-            foto2.classList.remove('closeFoto');
-        }, 0010);
-    }
-});
-
-// const sidebar = document.querySelector('.content-sidebar');
-// sidebar.addEventListener('click', (e) => {
-//     if (e.target !== foto1) {
-//         // console.log(e.target);
-//         // console.log('done');
-//         foto1.classList.add('closeFoto');
-//         foto1.classList.remove('openFoto');
-//         setTimeout(() => {
-//             foto1.classList.remove('closeFoto');
-//         }, 0010);
-//     }
-// });
 
 //! 2 foto==========
 foto2.addEventListener('click', () => {
@@ -110,87 +85,82 @@ li2.addEventListener('click', () => {
     }, 0010);
 });
 
-closeFotoBtn2.addEventListener('click', () => {
-    foto2.classList.add('closeFoto');
-    foto2.classList.remove('openFoto');
-    setTimeout(() => {
-        foto2.classList.remove('closeFoto');
-    }, 0010);
-});
 //! /OPEN FOTO
 // foto.addEventListener('mouseover', () => {
-//     foto.classList.add('hover');
+//     foto.classList.add('hover-foto');
 //     li.style.color = 'red';
 // });
-foto1.onmouseover = function () {
-    foto1.classList.add('hover');
-    li1.style.color = 'rgb(255, 208, 0)';
+
+foto1.addEventListener('mouseover', () => {
+    foto1.classList.add('hover-foto');
+    li1.classList.add('hover-li');
+});
+
+li1.onmouseout = function () {
+    foto1.classList.remove('hover-foto');
+    li1.classList.remove('hover-li');
 };
 foto1.onmouseout = function () {
-    foto1.classList.remove('hover');
-    li1.style.color = 'white';
+    foto1.classList.remove('hover-foto');
+    li1.classList.remove('hover-li');
+};
+li1.onmouseover = function () {
+    foto1.classList.add('hover-foto');
+    li1.classList.add('hover-li');
 };
 
-li1.onmouseover = function () {
-    foto1.classList.add('hover');
-    li1.style.color = 'rgb(255, 208, 0)';
-};
-li1.onmouseout = function () {
-    foto1.classList.remove('hover');
-    li1.style.color = 'white';
-};
 // ++++
 foto2.onmouseover = function () {
-    this.classList.add('hover');
+    this.classList.add('hover-foto');
     li2.style.color = 'rgb(255, 208, 0)';
 };
 foto2.onmouseout = function () {
-    this.classList.remove('hover');
+    this.classList.remove('hover-foto');
     li2.style.color = 'white';
 };
 
 li2.onmouseover = function () {
-    foto2.classList.add('hover');
+    foto2.classList.add('hover-foto');
     li2.style.color = 'rgb(255, 208, 0)';
 };
 li2.onmouseout = function () {
-    foto2.classList.remove('hover');
+    foto2.classList.remove('hover-foto');
     li2.style.color = 'white';
 };
 // ++++
 foto3.onmouseover = function () {
-    this.classList.add('hover');
+    this.classList.add('hover-foto');
     li3.style.color = 'rgb(255, 208, 0)';
 };
 foto3.onmouseout = function () {
-    this.classList.remove('hover');
+    this.classList.remove('hover-foto');
     li3.style.color = 'white';
 };
 
 li3.onmouseover = function () {
-    foto3.classList.add('hover');
+    foto3.classList.add('hover-foto');
     li3.style.color = 'rgb(255, 208, 0)';
 };
 li3.onmouseout = function () {
-    foto3.classList.remove('hover');
+    foto3.classList.remove('hover-foto');
     li3.style.color = 'white';
 };
 // ++++++
 foto4.onmouseover = function () {
-    this.classList.add('hover');
+    this.classList.add('hover-foto');
     li4.style.color = 'rgb(255, 208, 0)';
 };
 foto4.onmouseout = function () {
-    this.classList.remove('hover');
+    this.classList.remove('hover-foto');
     li4.style.color = 'white';
 };
 
 li4.onmouseover = function () {
-    foto4.classList.add('hover');
+    foto4.classList.add('hover-foto');
     li4.style.color = 'rgb(255, 208, 0)';
 };
 li4.onmouseout = function () {
-    foto4.classList.remove('hover');
+    foto4.classList.remove('hover-foto');
     li4.style.color = 'white';
 };
 // foto4.addEventListener('click', function (e) {
